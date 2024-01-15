223a132f947e28f53310b1e06a98b664ead9570a
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-15 17:21:45 -0500

    async code

--- index.js
+++ index.js
@@ -1,28 +1,6 @@
-const person = {
-  name: "Pranav",
-  age: 27,
-  introduce() {
-    console.log("Hi, this is " + this.name);
-  },
-};
+setTimeout(() => {
+  console.log("completed!");
+}, 2000);
 
-// const printName = (p) => {
-//   console.log(p.name);
-// };
-
-// printName(person);
-
-const printName = ({ name }) => {
-  console.log(name);
-};
-
-printName(person);
-
-const { name, age } = person;
-console.log(name);
-console.log(age);
-
-const fruits = ["apple", "orange", "grapes"];
-const [f1, f2] = fruits;
-
-console.log(f1, f2);
+console.log("Hello");
+console.log("Hi!");
