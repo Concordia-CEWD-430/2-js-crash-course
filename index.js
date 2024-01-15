71d944fc6a2977d77cacf045817718f4ad748445
PranavBhatia <bhatiapranav1996@gmail.com>
2024-01-14 23:33:03 -0500

    Arrow Functions

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 let itemPrice = 100;
 let taxAmount = 15;
 
-function printBill(price, tax) {
-    return "Your total will be $" + (price + tax) + ".";
-}
+const printBill = (price, tax) => {
+  return "Your total will be $" + (price + tax) + ".";
+};
+
+// const add = (a, b) => {
+//     return a + b;
+// }
+
+const add = (a, b) => a + b;
+
+console.log(add(2, 3));
 
 console.log(printBill(itemPrice, taxAmount));
